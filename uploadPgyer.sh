e37914e615f71d95893d703d48c9c2fdbae21258
caiguoqing <caigq@samsundot.com>
2019-03-13 18:56:22 +0800

    测试删除github根目录文件--uploadPayer

--- uploadPgyer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# author:zhangteng709
-
-current_git_branch_latest_log=`git log -n 1`
-
-lasteCommitlog=${current_git_branch_latest_log##*Date:}
-
-curl -F "file=@"$1"" -F "_api_key="$3"" "buildUpdateDescription="$lasteCommitlog"" http://www.pgyer.com/apiv2/app/upload
-
