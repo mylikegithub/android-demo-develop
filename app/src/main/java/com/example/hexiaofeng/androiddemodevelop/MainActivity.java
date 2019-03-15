484206e509d305c7529e659cd854e2d8a372fc32
caiguoqing <caigq@samsundot.com>
2019-03-15 09:54:53 +0800

    新增toast 33

--- app/src/main/java/com/example/hexiaofeng/androiddemodevelop/MainActivity.java
+++ app/src/main/java/com/example/hexiaofeng/androiddemodevelop/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this, "11", Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this, "11", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "22", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "33", Toast.LENGTH_SHORT).show();
     }
 }
